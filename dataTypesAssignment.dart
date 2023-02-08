dccefc7d2301da8dd06910ae42b0929ccbce20ce
Jack-tech707 <59619700+Jack-tech707@users.noreply.github.com>
2023-02-08 03:32:18 -0800

    Add files via upload

---
+++ dataTypesAssignment.dart
@@ -0,0 +1,17 @@
+//Jackson Mbugua
+//Assignment: implement the data types you have learned in the previous lesson
+//Data types : int, double, string, list
+void main() {
+  //int
+  int x=5;
+  //double
+  double y=  50.4;
+  //string 
+  String car = "Chevrolet";
+  //list- 
+  List wildAnimals=['Lion','Elephant','Giraffe','Ostrich','Hyena','Bufallo'];
+  //printing the data
+  print(x+y);// sum of x and y
+  print(car);
+  print(wildAnimals);
+}
